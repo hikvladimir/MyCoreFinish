a12ba2bfa4d86b99e602a3b3a056819a14dc173a
hikvladimir <psihouse10@gmail.com>
2024-09-25 22:31:20 +0300

    day2

---
+++ Dai2/operations.java
@@ -0,0 +1,22 @@
+/*
+* Создать проект, имя operations
+	В методы main создать все численные переменные
+	Инициализировать их начальными значениями
+	Провести математические операции над ними (не только с одниковыми, но и например int + long и тд)
+	Создать переменные под результаты операций
+	Результаты вывести в консоль*/
+public class operations {
+    public static void main(String[] args) {
+      int x=34;
+      long y=56L;
+      short z=256;
+      byte b=4;
+      float f=23.5f;
+      double d=456.003;
+
+      int reslt1=x+b;
+      long result2=y-z;
+        System.out.println(result2);
+        System.out.println(reslt1);
+    }
+}
